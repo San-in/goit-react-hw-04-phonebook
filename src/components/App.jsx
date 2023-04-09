9b4c8480f36fccc2fa318db61538844253023275
Sanin Oleksandr <saninalexsanin@gmail.com>
2023-04-10 00:13:04 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import React from 'react';
 
 import { ContactForm } from 'components/ContactForm/ContactForm';
